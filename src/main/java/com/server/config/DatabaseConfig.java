f6eac2bd75c08510e11e47d76a7ac50437675ffd
tmvf <tmvf@yandex.ru>
2017-05-10 21:18:04 +0400

    properties

---
+++ src/main/java/com/server/config/DatabaseConfig.java
@@ -0,0 +1,63 @@
+package com.server.config;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+@Configuration
+@EnableJpaRepositories("com.server.repository")
+@EnableTransactionManagement
+@PropertySource("classpath:db.properties")
+@ComponentScan("com.server")
+public class DatabaseConfig {
+
+    @Resource
+    private Environment env;
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean() {
+        LocalContainerEntityManagerFactoryBean em =
+                new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource());
+        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        em.setJpaProperties(getHibernateProperties());
+        return em;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl(env.getRequiredProperty("db.url"));
+        dataSource.setDriverClassName(env.getRequiredProperty("db.driver"));
+        dataSource.setUsername(env.getRequiredProperty("db.username"));
+        dataSource.setPassword(env.getRequiredProperty("db.password"));
+        return dataSource;
+    }
+
+    public Properties getHibernateProperties() {
+        Properties properties=new Properties();
+        InputStream stream=getClass().getClassLoader().
+                getResourceAsStream("hibernate.properties");
+        try {
+            properties.load(stream);
+            return properties;
+        } catch (IOException e) {
+            throw  new IllegalArgumentException("-----",e);
+        }
+
+
+    }
+}
--- src/main/java/com/server/controler/ReminderControler.java
+++ src/main/java/com/server/controler/ReminderControler.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/remind")
+@RequestMapping("/reminder")
 public class ReminderControler {
 
     @RequestMapping(value = "/get" ,method = RequestMethod.GET)
