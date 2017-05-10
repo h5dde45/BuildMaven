9e5a17b72748ba1a0afe7dd658ed7983ba4956da
tmvf <tmvf@yandex.ru>
2017-05-10 22:44:37 +0400

    Конфигурация Entity

--- src/main/java/com/server/config/DatabaseConfig.java
+++ src/main/java/com/server/config/DatabaseConfig.java
@@ -7,8 +7,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.annotation.Resource;
@@ -32,6 +34,7 @@ public class DatabaseConfig {
         LocalContainerEntityManagerFactoryBean em =
                 new LocalContainerEntityManagerFactoryBean();
         em.setDataSource(dataSource());
+        em.setPackagesToScan(env.getRequiredProperty("db.entity.package"));
         em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
         em.setJpaProperties(getHibernateProperties());
         return em;
@@ -47,15 +50,22 @@ public class DatabaseConfig {
         return dataSource;
     }
 
+    @Bean
+    public PlatformTransactionManager platformTransactionManager() {
+        JpaTransactionManager manager = new JpaTransactionManager();
+        manager.setEntityManagerFactory(entityManagerFactoryBean().getObject());
+        return manager;
+    }
+
     public Properties getHibernateProperties() {
-        Properties properties=new Properties();
-        InputStream stream=getClass().getClassLoader().
+        Properties properties = new Properties();
+        InputStream stream = getClass().getClassLoader().
                 getResourceAsStream("hibernate.properties");
         try {
             properties.load(stream);
             return properties;
         } catch (IOException e) {
-            throw  new IllegalArgumentException("-----",e);
+            throw new IllegalArgumentException("-----", e);
         }
 
 
