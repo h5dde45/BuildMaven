b8433006cb22353c1b0e8ee08afe35088fca4d6e
tmvf <tmvf@yandex.ru>
2017-05-10 15:47:49 +0400

    config

---
+++ src/main/java/com/server/controler/ReminderControler.java
@@ -0,0 +1,17 @@
+package com.server.controler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/remind")
+public class ReminderControler {
+
+    @RequestMapping(value = "/get" ,method = RequestMethod.GET)
+    @ResponseBody
+    public String getrem(){
+        return "33333";
+    }
+}
